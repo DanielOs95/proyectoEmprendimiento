e6ceeb92d2d519bec73344453854bba65552c93f
Daniel <espartan.95.os@gmail.com>
2025-12-14 21:28:27 -0600

    14122025

--- frontend/scriptCarrito.js
+++ frontend/scriptCarrito.js
@@ -2,44 +2,19 @@
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
-
-/*MOSTRAR Y OCULTAR CARRITO DE COMPRAS
-let shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#cart-btn').onclick = () => {
-    shoppingCart.classList.toggle('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
-}*/
-
-/*MOSTRAR Y OCULTAR REGISTRO DE USUARIOS
-let loginForm = document.querySelector('.login-form');
-document.querySelector('#login-btn').onclick = () => {
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    navbar.classList.remove('active');
-}*/
 
 /*MOSTRAR Y OCULTAR MENU HAMBURGUESA*/
 let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
 }
-
 
 /*OCULTAR SI OTRA OPCION ESTA DESPLEGADO*/
 window.onscroll = () => {
     searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
 
